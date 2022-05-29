b4e0f0b267d0eca02095245b5b1d8be1bd726571
kavyabs84 <97089467+Kavyabs84@users.noreply.github.com>
2022-05-29 12:00:50 +0000

    done

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,2 +1,13 @@
 # Dictionaries
+fname = input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    words_split = line.split()
+    for word in words_split:
+        if word not in lst:
+            lst.append(word)
 
+     
+lst.sort()
+print(lst)
--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,23 @@
 # Tuples
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+try:    
+    fh = open(fname)
+except:
+    print("file not exist")
 
-filename = "dataset/mbox-short.txt"
+count = 0
+
+for line in fh:
+     if not line.startswith("from"):
+        continue
+     if line.startswith("from"):
+        continue
+     words = line.split()
+     count = count+1
+     print(words[1])
+    
+
+print("There were", count, "lines in the file with From as the first word")
+
