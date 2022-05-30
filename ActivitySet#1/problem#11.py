c0169abeef43c5115692350e9b64d5a955a9bfb2
kavyabs84 <97089467+Kavyabs84@users.noreply.github.com>
2022-05-30 18:32:50 +0000

    done

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,7 +1,6 @@
 # Tuples
 fname = input("Enter file name: ")
-if len(fname) < 1:
-    fname = "mbox-short.txt"
+    
 try:    
     fh = open(fname)
 except:
@@ -10,14 +9,13 @@ except:
 count = 0
 
 for line in fh:
-     if not line.startswith("from"):
-        continue
-     if line.startswith("from"):
-        continue
-     words = line.split()
-     count = count+1
-     print(words[1])
     
+	if line.startswith("From "):
+		words = line.split()
+		count = count+1
+		print(words[1])
+	else:
+		continue
+                 
 
 print("There were", count, "lines in the file with From as the first word")
-
