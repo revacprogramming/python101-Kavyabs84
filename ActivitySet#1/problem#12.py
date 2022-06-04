bc0852440ce1c8c2bb9be8782978ca6fe514a02e
Kavyabs84 <manukavya987@gmail.com>
2022-06-04 23:05:35 +0530

    done

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,25 +1,32 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
 name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
 try:
-	handle = open(name)
+    handle = open(name)
 except:
-    print("entered file is invaild")
-count ={}
-lst=list()
+    print("file doesn't exist")
+    
+count = dict()
+lst = list()
+
 for line in handle:
-	if line.startswith("From: "):
-		line = line.split()
-		lst.append(line[1])
-	else:
-		continue        
-for word in lst:
-	count[word] = count.get(word,0)+1
-#print(words)
+    if not line.startswith("From:"):
+        continue
+    else:
+        line=line.split()
+        lst.append(line[1])
+    
+for words in lst:
+    count[words] = count.get(words,0) + 1
+
 bigcount = None
 bigword = None
-for word,val in count.items():
-    if bigcount is None or val > bigcount:
-        bigcount = val
+
+for word,value in count.items():
+    if bigcount is None or value > bigcount:
+        bigcount = value
         bigword = word
-print(bigword , bigcount)	
+        
+print(bigword,bigcount)
